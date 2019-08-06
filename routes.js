6e8ff98ea7dd8a8bef76a2c1a9f6f0a48dd41695
DanielChisica <danielchisica777@gmail.com>
2019-08-06 18:00:00 +0000

    Implemenying styles

--- routes.js
+++ routes.js
@@ -22,7 +22,7 @@ const subject2={
     'height':'170'
 }
 
-const subject3={
+const subject4={
     'name':'Natalia Irene',
     'surname':'Torres',
     'likes':'Watch anime, Listen crossover music, make home jobs',
@@ -33,12 +33,12 @@ const subject3={
     'height':'163'
 }
 
-const subject4={
+const subject3={
     'name':'Jose Luis',
     'surname':'Carrillo',
     'likes':'Watch MMA fights, sports',
-    'leisure':'Watch thriller movies, go to beefcake with friends, play basketball',
-    'description':'',
+    'leisure':'Watch thriller movies, go to barbecues with friends, play basketball',
+    'description':'Jose Luis is a funny guy, who is very disciplined in his job and good as job partner',
     'age':'35',
     'height':'180'
 }
@@ -60,15 +60,14 @@ http.createServer((request,response)=>{
                 let htmltext=data.toString();
                 if(index>0){
                     let interpolate=htmltext.match(/[^\{\}]+(?=\})/g)
-                    let name=eval(('subject'+index).name)
-                    let surname=eval(('subject'+index).surname)
-                    let likes=eval(('subject'+index).likes)
-                    let leisure=eval(('subject'+index).leisure)
-                    let description=eval(('subject'+index).description)
-                    let age=eval(('subject'+index).age)
-                    let height=eval(('subject'+index).height)
-
-                    for(let i=interpolate.length-1;i<=0;i--) {
+                    let name=eval('subject'+index).name
+                    let surname=eval('subject'+index).surname
+                    let likes=eval('subject'+index).likes
+                    let leisure=eval('subject'+index).leisure
+                    let description=eval('subject'+index).description
+                    let age=eval('subject'+index).age
+                    let height=eval('subject'+index).height
+                    for(let i=interpolate.length-1;i>=0;i--) {
                         let value=eval(interpolate[i])
                         console.log(value)
                         htmltext=htmltext.replace("{"+interpolate[i]+"}",value)
